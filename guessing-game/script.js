c5b5e168a4d5f0d7b8deba7a793b3740029561cb
Xandervdb1 <xandervandenbossche0@gmail.com>
2023-01-18 10:35:01 +0100

    Add user input for number

--- guessing-game/script.js
+++ guessing-game/script.js
@@ -1,3 +1,4 @@
+//A secret number will be generated. To make things fair, the number should be between 1 and (learners in the class)
 const learners = ['Ada','Basile', 'Beatrice', 'Céline', 'Claire', 'David', 'Freke', 'James', 'Jean', 'Jitske', 'Jordy', 'Juraj', 'Lisa', 'Marieke', 'Nick', 'Niels', 'Pious', 'Rachid', 'Raoul', 'Sander', 'Sarah', 'Seppe', 'Sofie', 'Sven', 'Tesse', 'Thomas', 'Toon', 'Vincent', 'Ward', 'Xander', 'Yousra'];
 let maxNumber = learners.length;
 
@@ -7,3 +8,5 @@ function getRandomInt(max) {
 
 let randomNumber = getRandomInt(maxNumber);
 console.log(randomNumber);
+
+//A user can enter a guess (what html element can be useful for this?)
